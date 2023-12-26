d5813811a79084bf67ec23fd10033b7ee57af363
JuniorJM <jimenezmoralesjunior@gmail.com>
2023-12-26 14:02:43 -0500

    agregando el dominio al archivo vite.config para que se despliegue en produccion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://braviasystems.000webhostapp.com/apps/licoreria-jankot/licoreria-jankot/', // Reemplaza con la ruta base de tu aplicación
   plugins: [react()],
 })
